d7af27d11edbdc35fa1329e007cd7dee815c010c
aeriihia <anni.riihiaho@helsinki.fi>
2022-06-21 02:43:54 +0300

    1.07

--- log-output/index.js
+++ log-output/index.js
@@ -1,3 +1,7 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+
 const hash = Math.random().toString(36).substr(2, 6)
 
 const printHash = () => {
@@ -6,3 +10,12 @@ const printHash = () => {
 }
 
 printHash()
+
+app.get('/', (req, res) => {
+  res.send(Date() + ' ' + hash)
+})
+  
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
+
