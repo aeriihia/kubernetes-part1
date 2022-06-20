4cdd3ff2ef5993fa32b7b7d3388ebd33573746e4
aeriihia <anni.riihiaho@helsinki.fi>
2022-06-20 03:29:27 +0300

    1.02

---
+++ project/backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
