97ad61b68ecb463f28e4ccd0b52ce2fb06d9e7a8
aeriihia <anni.riihiaho@helsinki.fi>
2022-06-22 06:59:02 +0300

    1.11

--- log-output/log-output-reader/index.js
+++ log-output/log-output-reader/index.js
@@ -5,10 +5,15 @@ const fs = require('fs/promises')
 const path = require('path')
 
 const directory = path.join('/', 'usr', 'src', 'app', 'files')
-const filePath = path.join(directory, 'timestamp.txt')
+const filePath1 = path.join(directory, 'timestamp.txt')
+const filePath2 = path.join(directory, 'pingpong.txt')
 
 const fileAlreadyExists = async () => new Promise(res => {
-  fs.stat(filePath, (err, stats) => {
+  fs.stat(filePath1, (err, stats) => {
+    if (err || !stats) return res(false)
+    return res(true)
+  })
+  fs.stat(filePath2, (err, stats) => {
     if (err || !stats) return res(false)
     return res(true)
   })
@@ -22,10 +27,11 @@ const findAFile = async () => {
 const hash = Math.random().toString(36).substr(2, 6)
 
 let timestamp_hash = ''
+let pingpongs = ''
 
 async function readTimestamp() {
   try {
-    const timestamp = await fs.readFile(filePath)
+    const timestamp = await fs.readFile(filePath1)
     timestamp_hash = timestamp + ' ' + hash
     console.log(timestamp_hash)
   } catch (err) {
@@ -34,11 +40,22 @@ async function readTimestamp() {
   setTimeout(readTimestamp, 5000)
 }
 
+async function readPingPongs() {
+  try {
+    pingpongs = await fs.readFile(filePath2)
+    console.log(pingpongs)
+  } catch (err) {
+    console.log(err)
+  }
+  setTimeout(readPingPongs, 5000)
+}
+
 findAFile()
 readTimestamp()
+readPingPongs()
 
 app.get('/', (req, res) => {
-  res.send(timestamp_hash)
+  res.send(`<html>${timestamp_hash}<br/>${pingpongs}</html>`)
 })
   
 app.listen(PORT, () => {
