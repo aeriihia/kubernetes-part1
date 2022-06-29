79576cfa2dc4cbac6d70ad6e48f0e56331bb37b6
aeriihia <anni.riihiaho@helsinki.fi>
2022-06-29 19:32:57 +0300

    1.13

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
   const [image, setImage] = useState(null)
@@ -17,11 +18,26 @@ function App() {
   useEffect(() => {
     fetchImage()
   }, [])
+  
+  const todos = [{ "id":1, "content":"TODO 1" }, { "id":2, "content":"TODO 2" }]
+  
+  const addToDo = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
 
   return (
     <div>
       {image && <img src={image} height={300} width={300} alt="picsum" />}
-      <p>Hello frontend</p>
+      <form onSubmit={addToDo}>
+        <input maxlength="140" />
+        <button type="submit">Create TODO</button>
+      </form>
+      <ul>
+        {todos.map(todo => 
+          <li key={todo.id}>{todo.content}</li>
+        )}
+      </ul>
     </div>
   )
 }
